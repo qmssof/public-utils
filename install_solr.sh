bf6054d7e2fc3e0bdd48447d531a0564ea973de3
Philippe Duval <philippe.duval@qms-software.com>
2022-01-05 18:08:22 +0000

    add solr as a systemd service

--- install_solr.sh
+++ install_solr.sh
@@ -15,3 +15,26 @@ wget "${ARCHIVE_URL}" -O ${ARCHIVE_NAME}
 tar -xvf ${ARCHIVE_NAME}
 sudo ${DIR_NAME}/bin/install_solr_service.sh ${ARCHIVE_NAME} -d /solrdata
 
+cat <<EOT >> solr.service
+[Unit]
+Description=Apache SOLR
+
+[Service]
+Type=forking
+User=solr
+Environment=SOLR_INCLUDE=/etc/default/solr.in.sh
+ExecStart=/opt/solr/bin/solr start
+ExecStop=/opt/solr/bin/solr stop
+Restart=on-failure
+LimitNOFILE=65000
+LimitNPROC=65000
+TimeoutSec=180s
+
+[Install]
+WantedBy=multi-user.target
+EOT
+sudo mv solr.service /etc/systemd/system/
+sudo systemctl stop solr
+sudo systemctl daemon-reload
+sudo systemctl enable solr
+sudo systemctl start solr
